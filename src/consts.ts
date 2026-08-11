32061f5a53fd79569a96becd04b937c0bd5ec9d3
DrMrProfessor <230793134+DrMrProfessor@users.noreply.github.com>
2026-08-11 10:06:49 +1000

    personalise site title and clear sample posts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'sidequesting';
+export const SITE_DESCRIPTION = 'Building in public - homelab, cybersecurity and many side quests along the way!';
